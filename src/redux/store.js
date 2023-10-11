25d84c0c7130e5728979bf715538e051b868da3d
mariluzv88 <59103912+mariluzv88@users.noreply.github.com>
2023-10-11 11:02:47 -0400

    updated redux tool kit

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,6 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from './userSlice'
 
 const rootReducer = combineReducers({user: userReducer})
-const store = configureStore({
-    reducer:
+ export const store = configureStore({
+    reducer: rootReducer
 })
